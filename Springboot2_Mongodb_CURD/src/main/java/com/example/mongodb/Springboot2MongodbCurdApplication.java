f0e702e2ccd53ed052a66c604037369cc3f929f3
manish <manish@LAPTOP-0DTRRONF>
2019-08-13 23:39:11 +0530

    test

---
+++ Springboot2_Mongodb_CURD/src/main/java/com/example/mongodb/Springboot2MongodbCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MongodbCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2MongodbCurdApplication.class, args);
+	}
+
+}
